0b0f3646cfdfc153dc9ed2c90b7b75bcaf1097e3
sushant suwal <sushantdsuwal@gmail.com>
2019-03-19 10:53:00 +0545

    fix for web

--- src/app/screens/index.js
+++ src/app/screens/index.js
@@ -1,4 +1,4 @@
 import ListScreen from './ListScreen';
-import ProfileScreen from './ProfileScreen'
+// import ProfileScreen from './ProfileScreen'
 
-export { ListScreen, ProfileScreen };
+export { ListScreen };
